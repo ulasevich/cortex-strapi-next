95d1cb3d8f250d740bc9f85236c77cf37622bcdc
ulasevich <aleksandr@spellsystems.com>
2023-06-04 21:46:41 +0300

    env prod upd

--- backend/config/env/production/database.ts
+++ backend/config/env/production/database.ts
@@ -2,6 +2,7 @@ module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
+      connectionString: env('DATABASE_URL'),
       host: env('DATABASE_HOST'),
       port: env.int('DATABASE_PORT'),
       database: env('DATABASE_NAME'),
