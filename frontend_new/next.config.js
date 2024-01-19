77ea1f78f156fe299d3fc549f355b27272f3a6bf
ulasevich <aleksandr@spellsystems.com>
2024-01-19 16:40:49 +0300

    update

--- frontend_new/next.config.js
+++ frontend_new/next.config.js
@@ -1,19 +1,31 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // reactStrictMode: true, // может быть нужен для dev режима, может производить рендер 2 раза
+    reactStrictMode: true, // может быть нужен для dev режима, может производить рендер 2 раза
     output: 'standalone',
     images: {
-        // domains: ["localhost", "learn.microsoft.com"],
+        // domains: ["localhost", "learn.microsoft.com", "cx-test.com"],
         remotePatterns: [
             {
                 protocol: 'http',
                 hostname: 'localhost',
                 port: '1337',
-                pathname: '/uploads/**',
+                pathname: 'localhost:1337/',
+            },
+            {
+                protocol: 'http',
+                hostname: 'host.docker.internal',
+                port: '1337',
+            },
+            {
+                protocol: 'http',
+                hostname: '127.0.0.1',
+                port: '1337',
+                pathname: '127.0.0.1:1337/**',
             },
             {
                 protocol: 'http',
                 hostname: 'cx-test.com',
+                pathname: '/uploads/**',
             },
             {
                 protocol: 'https',
